467e22732df057f50a96851d0f18483a59572b3a
Haotian <huntchen00@gmail.com>
2025-07-28 13:06:04 +0800

    fix: add analytics

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -42,6 +42,7 @@ export default async function LocaleLayout({
             {children}
           </NextIntlClientProvider>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
